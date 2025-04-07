bc92a366d09af1d20f1e304125ec82d2fdbdfc8d
Been2000 <106220288+samsam1568@users.noreply.github.com>
2025-04-07 12:35:52 +0900

    [Silver V] Title: 영화감독 숌, Time: 632 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,15 +1,16 @@
 import sys
 
 N = int(sys.stdin.readline().rstrip())
-a = 666
+a = 1
 cnt = 0
 while True:
     if '666' in str(a):
         cnt += 1
 
     if cnt == N:
+        print(a)
         break
     a += 1
-print(a)
+
 
 
