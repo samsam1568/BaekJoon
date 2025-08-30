4f63b764c93ca551daf031524b682420f99b50d3
Been2000 <106220288+samsam1568@users.noreply.github.com>
2025-08-30 16:39:01 +0900

    [Silver IV] Title: 체크포인트 달리기, Time: 348 ms, Memory: 41948 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/29891. 체크포인트 달리기/체크포인트 달리기.py
+++ 백준/Silver/29891. 체크포인트 달리기/체크포인트 달리기.py
@@ -1,15 +1,63 @@
 import sys
+
+def isPos(a):
+    check = 0
+    for i in a:
+        if i > 0:
+            check += 1
+
+    if check == len(a):
+        return True
+
+    return False
+
+def isNeg(a):
+    check = 0
+    for i in a:
+        if i < 0:
+            check += 1
+
+    if check == len(a):
+        return True
+
+    return False
+
+
+
 n, m = map(int, sys.stdin.readline().rstrip().split())
 arr = []
 for i in range(n):
     a = int(sys.stdin.readline().rstrip())
     arr.append(a)
-# arr = set(arr)
-# arr = list(arr)
 arr.sort()
 tot = 0
 k = n - 1
-while k >= 0:
-    tot += abs(arr[k]) * 2
-    k -= m
+
+while len(arr) >= m:
+    tmp = []
+    for k in range(m):
+        tmp.append(arr.pop())
+    if isPos(tmp):
+        tot += max(tmp)*2
+
+    elif isNeg(tmp):
+        tot += abs(min(tmp))*2
+
+    else:
+        tot += max(tmp)*2
+        tot += abs(min(tmp))*2
+
+if len(arr) > 0:
+    if isPos(arr):
+        tot += max(arr)*2
+
+    elif isNeg(arr):
+        tot += abs(min(arr))*2
+
+    else:
+        tot += max(arr)*2
+        tot += abs(min(arr))*2
+
 print(tot)
+
+
