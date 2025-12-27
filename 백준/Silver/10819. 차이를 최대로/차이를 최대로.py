a0b16982536472f97bdd4617990593cce43e4594
Been2000 <106220288+samsam1568@users.noreply.github.com>
2025-12-27 14:37:20 +0900

    [Silver II] Title: 차이를 최대로, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+
+per = permutations(a)
+ans = 0
+for i in per:
+    s = 0
+    for j in range(len(i) - 1):
+        s += abs(i[j] - i[j + 1])
+    if s > ans:
+        ans = s
+print(ans)
