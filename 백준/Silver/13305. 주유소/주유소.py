2ae006e815700b39c10acf6c52c05925cb4fa750
Been2000 <106220288+samsam1568@users.noreply.github.com>
2026-04-12 16:36:59 +0900

    [Silver III] Title: 주유소, Time: 84 ms, Memory: 44788 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,12 +1,15 @@
+import math
 import sys
 
 N = int(sys.stdin.readline())
 lLoad = list(map(int,sys.stdin.readline().split()))
 pOil = list(map(int,sys.stdin.readline().split()))
 ans = pOil[0] * lLoad[0]
+oil_min = math.inf
 
 for idx in range(1,N-1):
-    ans += min(pOil[:idx+1]) * lLoad[idx]
+    oil_min = min(oil_min,pOil[idx])
+    ans += oil_min * lLoad[idx]
 
 print(ans)
 
