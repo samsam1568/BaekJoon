4566fa4fa04572b7ed93dbe527755b784cd33623
Been2000 <106220288+samsam1568@users.noreply.github.com>
2026-05-20 19:38:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -0,0 +1,7 @@
+def solution(seoul):
+    answer = ''
+    for idx,value in enumerate(seoul):
+        if value == "Kim":
+            answer = "김서방은 " + str(idx) +"에 있다"
+            break
+    return answer
